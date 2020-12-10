8459c02e1b751b3aa4e81c43a15460a574e85d49
anton <kiyloanton@gmail.com>
2020-12-10 11:26:53 +0300

    Valid-Parentheses

---
+++ 5_kyu/Valid-Parentheses.js
@@ -0,0 +1,14 @@
+function validParentheses(parens){
+  let count = 0
+  for(let i = 0; i < parens.length; i++){
+    if(parens[i] === "(") count++
+    if(parens[i] === ")") count--
+    if(count < 0) return false
+  }
+  return count === 0
+}
+
+console.log(validParentheses("()")) // =>  true
+console.log(validParentheses(")(()))")) // =>  false
+console.log(validParentheses("(")) // =>  false
+console.log(validParentheses("(())((()())())")) // =>  true
