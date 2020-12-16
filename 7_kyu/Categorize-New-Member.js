4c569922df94f68425bed8eace2e09e8718ec7d9
anton <kiyloanton@gmail.com>
2020-12-16 11:28:53 +0300

    Categorize-New-Member

---
+++ 7_kyu/Categorize-New-Member.js
@@ -0,0 +1,15 @@
+function openOrSenior(data){
+  const output = []
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][0] >= 55 && data[i][1] > 7) {
+      output.push('Senior')
+    } else {
+      output.push('Open')
+    }
+  }
+  return output
+}
+
+console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]])) // => ['Open', 'Senior', 'Open', 'Senior']
+console.log(openOrSenior([[3, 12],[55,1],[91, -2],[54, 23]])) // => ['Open', 'Open', 'Open', 'Open']
+console.log(openOrSenior([[59, 12],[55,-1],[12, -2],[12, 12]])) // => ['Senior', 'Open', 'Open', 'Open']
